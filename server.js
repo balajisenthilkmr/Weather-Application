03c8499ebe87c5450c7c2bbf64c1001322871470
praghub <prakashbalan555@gmail.comgit config --global --listgit config --global user.email prakashbalan555@gmail.com>
2023-12-05 00:47:50 +0530

    update: created firestore database

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const db = require('./config/firebase/config').firestore();
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use('/', express.static(__dirname + '/assets'));
+app.set('view engine','ejs');
+app.set('views','./views')
+
+app.get('/',(req, res) => {res.render('index')})
+
+app.post("/history",async (req, res) => {
+    console.log(req.body);
+    await db.collection('history').add({
+        ...req.body,
+        createdAt: new Date()
+    })
+    res.json({ status: 201, msg: "Successfully Created!"})
+})
+
+
+app.listen(9000, () => {console.log("Server is running... [PORT:9000]");});
